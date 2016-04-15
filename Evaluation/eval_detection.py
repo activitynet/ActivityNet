f0611aaa4a6e60498c3c97c568e3db4565f0e3d9
cabaf <fabiancabaheilbron@gmail.com>
2016-04-15 23:00:44 +0300

    [Challenge16] Added evaluation module for detection.

---
+++ Evaluation/eval_detection.py
@@ -0,0 +1,209 @@
+import json
+import urllib2
+
+import numpy as np
+import pandas as pd
+
+from utils import get_blocked_videos
+from utils import interpolated_prec_rec
+from utils import segment_iou
+
+class ANETdetection(object):
+
+    GROUND_TRUTH_FIELDS = ['database', 'taxonomy', 'version']
+    PREDICTION_FIELDS = ['results', 'version']
+
+    def __init__(self, ground_truth_filename=None, prediction_filename=None,
+                 ground_truth_fields=GROUND_TRUTH_FIELDS,
+                 prediction_fields=PREDICTION_FIELDS,
+                 tiou_thr=0.5, subset='validation', verbose=False):
+        if not ground_truth_filename:
+            raise IOError('Please input a valid ground truth file.')
+        if not prediction_filename:
+            raise IOError('Please input a valid prediction file.')
+        self.subset = subset
+        self.tiou_thr = tiou_thr
+        self.verbose = verbose
+        self.gt_fields = ground_truth_fields
+        self.pred_fields = prediction_fields
+        self.ap = None
+        # Retrieve blocked videos from server.
+        self.blocked_videos = get_blocked_videos()
+        # Import ground truth and predictions.
+        self.ground_truth, self.activity_index = self._import_ground_truth(
+            ground_truth_filename)
+        self.prediction = self._import_prediction(prediction_filename)
+
+        if self.verbose:
+            print '[INIT] Loaded annotations from {} subset.'.format(subset)
+            nr_gt = len(self.ground_truth)
+            print '\tNumber of ground truth instances: {}'.format(nr_gt)
+            nr_pred = len(self.prediction)
+            print '\tNumber of predictions: {}'.format(nr_pred)
+            print '\tFixed threshold for tiou score: {}'.format(self.tiou_thr)
+
+    def _import_ground_truth(self, ground_truth_filename):
+        """Reads ground truth file, checks if it is well formatted, and returns
+           the ground truth instances and the activity classes.
+
+        Parameters
+        ----------
+        ground_truth_filename : str
+            Full path to the ground truth json file.
+
+        Outputs
+        -------
+        ground_truth : df
+            Data frame containing the ground truth instances.
+        activity_index : dict
+            Dictionary containing class index.
+        """
+        with open(ground_truth_filename, 'r') as fobj:
+            data = json.load(fobj)
+        # Checking format
+        if not all([field in data.keys() for field in self.gt_fields]):
+            raise IOError('Please input a valid ground truth file.')
+
+        # Initialize data frame
+        columns=['video-id', 't-start', 't-end', 'label']
+        ground_truth = pd.DataFrame(columns=columns)
+        activity_index, cidx = {}, 0
+        idx = 0
+        for videoid, v in data['database'].iteritems():
+            if self.subset != v['subset']:
+                continue
+            if videoid in self.blocked_videos:
+                continue
+            for ann in v['annotations']:
+                if ann['label'] not in activity_index:
+                    activity_index[ann['label']] = cidx
+                    cidx += 1
+                ground_truth.loc[idx] = {'video-id': videoid,
+                                         't-start': ann['segment'][0],
+                                         't-end': ann['segment'][1],
+                                         'label': activity_index[ann['label']]}
+                idx += 1
+        return ground_truth, activity_index
+
+    def _import_prediction(self, prediction_filename):
+        """Reads prediction file, checks if it is well formatted, and returns
+           the prediction instances.
+
+        Parameters
+        ----------
+        prediction_filename : str
+            Full path to the prediction json file.
+
+        Outputs
+        -------
+        prediction : df
+            Data frame containing the prediction instances.
+        """
+        with open(prediction_filename, 'r') as fobj:
+            data = json.load(fobj)
+        # Checking format...
+        if not all([field in data.keys() for field in self.pred_fields]):
+            raise IOError('Please input a valid prediction file.')
+
+        # Initialize data frame
+        columns = ['video-id', 't-start', 't-end', 'label', 'score']
+        prediction = pd.DataFrame(columns=columns)
+        idx = 0
+        for videoid, v in data['results'].iteritems():
+            if videoid in self.blocked_videos:
+                continue
+            for result in v:
+                label = self.activity_index[result['label']]
+                prediction.loc[idx] = {'video-id': videoid,
+                                       't-start': result['segment'][0],
+                                       't-end': result['segment'][1],
+                                       'label': label,
+                                       'score': result['score']}
+                idx += 1
+        return prediction
+
+    def wrapper_compute_average_precision(self):
+        """Computes average precision for each class in the subset.
+        """
+        ap = np.zeros(len(self.activity_index.items()))
+        for activity, cidx in self.activity_index.iteritems():
+            gt_idx = self.ground_truth['label'] == cidx
+            pred_idx = self.prediction['label'] == cidx
+            ap[cidx] = compute_average_precision_detection(
+                self.ground_truth.loc[gt_idx].reset_index(drop=True),
+                self.prediction.loc[pred_idx].reset_index(drop=True),
+                tiou_thr=self.tiou_thr)
+        return ap
+
+    def evaluate(self):
+        """Evaluates a prediction file. For the detection task we measure the
+        interpolated mean average precision to measure the performance of a
+        method.
+        """
+        ap = self.wrapper_compute_average_precision()
+        if self.verbose:
+            print '[RESULTS] Mean Average Precision: {}'.format(ap.mean())
+        self.ap = ap
+
+def compute_average_precision_detection(ground_truth, prediction, tiou_thr=0.5):
+    """Compute average precision (detection task) between ground truth and
+    predictions data frames. If multiple predictions occurs for the same
+    predicted segment, only the one with highest score is matches as
+    true positive. This code is greatly inspired by Pascal VOC devkit.
+
+    Parameters
+    ----------
+    ground_truth : df
+        Data frame containing the ground truth instances.
+        Required fields: ['video-id', 't-start', 't-end']
+    prediction : df
+        Data frame containing the prediction instances.
+        Required fields: ['video-id, 't-start', 't-end', 'score']
+    tiou_thr : (float, optional)
+        Temporal intersection over union threshold.
+
+    Outputs
+    -------
+    ap : float
+        Average precision score.
+    """
+    npos = float(len(ground_truth))
+    lock_gt = np.ones(len(ground_truth)) * -1
+    # Sort predictions by decreasing score order.
+    sort_idx = prediction['score'].values.argsort()[::-1]
+    prediction = prediction.loc[sort_idx].reset_index(drop=True)
+
+    # Initialize true positive and false positive vectors.
+    tp = np.zeros(len(prediction))
+    fp = np.zeros(len(prediction))
+
+    # Assigning true positive to truly grount truth instances.
+    for idx in range(len(prediction)):
+        this_pred = prediction.loc[idx]
+        gt_idx = ground_truth['video-id'] == this_pred['video-id']
+        # Check if there is at least one ground truth in the video associated.
+        if not gt_idx.any():
+            fp[idx] = 1
+            continue
+        this_gt = ground_truth.loc[gt_idx].reset_index()
+        tiou_arr = segment_iou(this_pred[['t-start', 't-end']].values,
+                               this_gt[['t-start', 't-end']].values)
+        # We would like to retrieve the predictions with highest tiou score.
+        tiou_sorted_idx = tiou_arr.argsort()[::-1]
+        for jdx in tiou_sorted_idx:
+            if tiou_arr[jdx] < tiou_thr:
+                fp[idx] = 1
+                break
+            if lock_gt[this_gt.loc[jdx]['index']] >= 0:
+                continue
+            # Assign as true positive after the filters above.
+            tp[idx] = 1
+            lock_gt[this_gt.loc[jdx]['index']] = idx
+            break
+
+    # Computing prec-rec
+    tp = np.cumsum(tp).astype(np.float)
+    fp = np.cumsum(fp).astype(np.float)
+    rec = tp / npos
+    prec = tp / (tp + fp)
+    return interpolated_prec_rec(rec, prec)
