f62408e196d228d4f32072b0c1b07b2407c53e40
HumamAlwassel <h.alwassel@gmail.com>
2017-04-19 23:04:08 +0300

    [Evaluation] Changed the evaluation metric for the detection task to average-mAP

--- Evaluation/eval_detection.py
+++ Evaluation/eval_detection.py
@@ -16,14 +16,15 @@ class ANETdetection(object):
     def __init__(self, ground_truth_filename=None, prediction_filename=None,
                  ground_truth_fields=GROUND_TRUTH_FIELDS,
                  prediction_fields=PREDICTION_FIELDS,
-                 tiou_thr=0.5, subset='validation', verbose=False, 
+                 tiou_thresholds=np.linspace(0.5, 0.95, 10), 
+                 subset='validation', verbose=False, 
                  check_status=True):
         if not ground_truth_filename:
             raise IOError('Please input a valid ground truth file.')
         if not prediction_filename:
             raise IOError('Please input a valid prediction file.')
         self.subset = subset
-        self.tiou_thr = tiou_thr
+        self.tiou_thresholds = tiou_thresholds
         self.verbose = verbose
         self.gt_fields = ground_truth_fields
         self.pred_fields = prediction_fields
@@ -45,7 +46,7 @@ class ANETdetection(object):
             print '\tNumber of ground truth instances: {}'.format(nr_gt)
             nr_pred = len(self.prediction)
             print '\tNumber of predictions: {}'.format(nr_pred)
-            print '\tFixed threshold for tiou score: {}'.format(self.tiou_thr)
+            print '\tFixed threshold for tiou score: {}'.format(self.tiou_thresholds)
 
     def _import_ground_truth(self, ground_truth_filename):
         """Reads ground truth file, checks if it is well formatted, and returns
@@ -135,14 +136,14 @@ class ANETdetection(object):
     def wrapper_compute_average_precision(self):
         """Computes average precision for each class in the subset.
         """
-        ap = np.zeros(len(self.activity_index.items()))
+        ap = np.zeros((len(self.tiou_thresholds), len(self.activity_index.items())))
         for activity, cidx in self.activity_index.iteritems():
             gt_idx = self.ground_truth['label'] == cidx
             pred_idx = self.prediction['label'] == cidx
-            ap[cidx] = compute_average_precision_detection(
+            ap[:,cidx] = compute_average_precision_detection(
                 self.ground_truth.loc[gt_idx].reset_index(drop=True),
                 self.prediction.loc[pred_idx].reset_index(drop=True),
-                tiou_thr=self.tiou_thr)
+                tiou_thresholds=self.tiou_thresholds)
         return ap
 
     def evaluate(self):
@@ -150,13 +151,13 @@ class ANETdetection(object):
         interpolated mean average precision to measure the performance of a
         method.
         """
-        ap = self.wrapper_compute_average_precision()
+        self.ap = self.wrapper_compute_average_precision()
+        self.mAP = self.ap.mean(axis=1)
         if self.verbose:
             print '[RESULTS] Performance on ActivityNet detection task.'
-            print '\tMean Average Precision: {}'.format(ap.mean())
-        self.ap = ap
+            print '\tAverage-mAP: {}'.format(self.mAP.mean())
 
-def compute_average_precision_detection(ground_truth, prediction, tiou_thr=0.5):
+def compute_average_precision_detection(ground_truth, prediction, tiou_thresholds=np.linspace(0.5, 0.95, 10)):
     """Compute average precision (detection task) between ground truth and
     predictions data frames. If multiple predictions occurs for the same
     predicted segment, only the one with highest score is matches as
@@ -170,7 +171,7 @@ def compute_average_precision_detection(ground_truth, prediction, tiou_thr=0.5):
     prediction : df
         Data frame containing the prediction instances.
         Required fields: ['video-id, 't-start', 't-end', 'score']
-    tiou_thr : (float, optional)
+    tiou_thresholds : 1darray, optional
         Temporal intersection over union threshold.
 
     Outputs
@@ -179,45 +180,56 @@ def compute_average_precision_detection(ground_truth, prediction, tiou_thr=0.5):
         Average precision score.
     """
     npos = float(len(ground_truth))
-    lock_gt = np.ones(len(ground_truth)) * -1
+    lock_gt = np.ones((len(tiou_thresholds),len(ground_truth))) * -1
     # Sort predictions by decreasing score order.
     sort_idx = prediction['score'].values.argsort()[::-1]
     prediction = prediction.loc[sort_idx].reset_index(drop=True)
 
     # Initialize true positive and false positive vectors.
-    tp = np.zeros(len(prediction))
-    fp = np.zeros(len(prediction))
+    tp = np.zeros((len(tiou_thresholds), len(prediction)))
+    fp = np.zeros((len(tiou_thresholds), len(prediction)))
+
+    # Adaptation to query faster
+    ground_truth_gbvn = ground_truth.groupby('video-id')
 
     # Assigning true positive to truly grount truth instances.
-    for idx in range(len(prediction)):
-        this_pred = prediction.loc[idx]
-        gt_idx = ground_truth['video-id'] == this_pred['video-id']
-        # Check if there is at least one ground truth in the video associated.
-        if not gt_idx.any():
-            fp[idx] = 1
+    for idx, this_pred in prediction.iterrows():
+
+        try:
+            # Check if there is at least one ground truth in the video associated.
+            ground_truth_videoid = ground_truth_gbvn.get_group(this_pred['video-id'])
+        except Exception as e:
+            fp[:, idx] = 1
             continue
-        this_gt = ground_truth.loc[gt_idx].reset_index()
+
+        this_gt = ground_truth_videoid.reset_index()
         tiou_arr = segment_iou(this_pred[['t-start', 't-end']].values,
                                this_gt[['t-start', 't-end']].values)
         # We would like to retrieve the predictions with highest tiou score.
         tiou_sorted_idx = tiou_arr.argsort()[::-1]
-        for jdx in tiou_sorted_idx:
-            if tiou_arr[jdx] < tiou_thr:
-                fp[idx] = 1
+        for tidx, tiou_thr in enumerate(tiou_thresholds):
+            for jdx in tiou_sorted_idx:
+                if tiou_arr[jdx] < tiou_thr:
+                    fp[tidx, idx] = 1
+                    break
+                if lock_gt[tidx, this_gt.loc[jdx]['index']] >= 0:
+                    continue
+                # Assign as true positive after the filters above.
+                tp[tidx, idx] = 1
+                lock_gt[tidx, this_gt.loc[jdx]['index']] = idx
                 break
-            if lock_gt[this_gt.loc[jdx]['index']] >= 0:
-                continue
-            # Assign as true positive after the filters above.
-            tp[idx] = 1
-            lock_gt[this_gt.loc[jdx]['index']] = idx
-            break
 
-        if fp[idx] == 0 and tp[idx] == 0:
-            fp[idx] = 1
+            if fp[tidx, idx] == 0 and tp[tidx, idx] == 0:
+                fp[tidx, idx] = 1
 
-    # Computing prec-rec
-    tp = np.cumsum(tp).astype(np.float)
-    fp = np.cumsum(fp).astype(np.float)
-    rec = tp / npos
-    prec = tp / (tp + fp)
-    return interpolated_prec_rec(prec, rec)
+    ap = np.zeros(len(tiou_thresholds))
+
+    for tidx in range(len(tiou_thresholds)):
+        # Computing prec-rec
+        this_tp = np.cumsum(tp[tidx,:]).astype(np.float)
+        this_fp = np.cumsum(fp[tidx,:]).astype(np.float)
+        rec = this_tp / npos
+        prec = this_tp / (this_tp + this_fp)
+        ap[tidx] = interpolated_prec_rec(prec, rec)
+
+    return ap
--- Evaluation/get_detection_performance.py
+++ Evaluation/get_detection_performance.py
@@ -1,11 +1,14 @@
 import argparse
+import numpy as np
 
 from eval_detection import ANETdetection
 
 def main(ground_truth_filename, prediction_filename,
-         subset='validation', tiou_thr=0.5, verbose=True, check_status=True):
+         subset='validation', tiou_thresholds=np.linspace(0.5, 0.95, 10),
+         verbose=True, check_status=True):
+
     anet_detection = ANETdetection(ground_truth_filename, prediction_filename,
-                                   subset=subset, tiou_thr=tiou_thr,
+                                   subset=subset, tiou_thresholds=tiou_thresholds,
                                    verbose=verbose, check_status=True)
     anet_detection.evaluate()
 
@@ -22,7 +25,7 @@ def parse_input():
     p.add_argument('--subset', default='validation',
                    help=('String indicating subset to evaluate: '
                          '(training, validation)'))
-    p.add_argument('--tiou_thr', type=float, default=0.5,
+    p.add_argument('--tiou_thresholds', type=float, default=np.linspace(0.5, 0.95, 10),
                    help='Temporal intersection over union threshold.')
     p.add_argument('--verbose', type=bool, default=True)
     p.add_argument('--check_status', type=bool, default=True)
