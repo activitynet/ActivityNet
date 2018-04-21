530ac3acc4d46cf8b1916599c6185224f17c6f54
Victor Escorcia <escorciav@gmail.com>
2018-04-22 02:41:34 +0300

    [Crawler] Validate Crawler for Kinetics-600
    Update crawler to support the download of Kinectics-600 and
    Kinectics-400.
    
    - Fix download of test set
    
    - Rename CSV files of Kinectics-400
    
    - Add CSV files of Kinetics-600
    
    - Minor refactoring

--- Crawler/Kinetics/download.py
+++ Crawler/Kinetics/download.py
@@ -1,11 +1,11 @@
 import argparse
-import fnmatch
 import glob
 import json
 import os
 import shutil
 import subprocess
 import uuid
+from collections import OrderedDict
 
 from joblib import delayed
 from joblib import Parallel
@@ -14,6 +14,12 @@ import pandas as pd
 
 def create_video_folders(dataset, output_dir, tmp_dir):
     """Creates a directory for each label name in the dataset."""
+    if 'label-name' not in dataset.columns:
+        this_dir = os.path.join(output_dir, 'test')
+        if not os.path.exists(this_dir):
+            os.makedirs(this_dir)
+        # I should return a dict but ...
+        return this_dir
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     if not os.path.exists(tmp_dir):
@@ -29,23 +35,27 @@ def create_video_folders(dataset, output_dir, tmp_dir):
 
 
 def construct_video_filename(row, label_to_dir, trim_format='%06d'):
-    """Given a dataset row, this function constructs the 
+    """Given a dataset row, this function constructs the
        output filename for a given video.
     """
     basename = '%s_%s_%s.mp4' % (row['video-id'],
                                  trim_format % row['start-time'],
                                  trim_format % row['end-time'])
-    output_filename = os.path.join(label_to_dir[row['label-name']], basename)
+    if not isinstance(label_to_dir, dict):
+        dirname = label_to_dir
+    else:
+        dirname = label_to_dir[row['label-name']]
+    output_filename = os.path.join(dirname, basename)
     return output_filename
 
 
 def download_clip(video_identifier, output_filename,
-                  start_time, end_time, 
+                  start_time, end_time,
                   tmp_dir='/tmp/kinetics',
                   num_attempts=5,
                   url_base='https://www.youtube.com/watch?v='):
     """Download a video from youtube if exists and is not blocked.
-    
+
     arguments:
     ---------
     video_identifier: str
@@ -53,7 +63,7 @@ def download_clip(video_identifier, output_filename,
     output_filename: str
         File path where the video will be stored.
     start_time: float
-        Indicates the begining time in seconds from where the video 
+        Indicates the begining time in seconds from where the video
         will be trimmed.
     end_time: float
         Indicates the ending time in seconds of the trimmed video.
@@ -70,21 +80,21 @@ def download_clip(video_identifier, output_filename,
     command = ['youtube-dl',
                '--quiet', '--no-warnings',
                '-f', 'mp4',
-               '-o', '"%s"' % tmp_filename, 
+               '-o', '"%s"' % tmp_filename,
                '"%s"' % (url_base + video_identifier)]
     command = ' '.join(command)
     attempts = 0
     while True:
-         try:
-            output = subprocess.check_output(command, shell=True, 
+        try:
+            output = subprocess.check_output(command, shell=True,
                                              stderr=subprocess.STDOUT)
-         except subprocess.CalledProcessError as err:
+        except subprocess.CalledProcessError as err:
             attempts += 1
             if attempts == num_attempts:
                 return status, err.output
-         else:
+        else:
             break
-            
+
     tmp_filename = glob.glob('%s*' % tmp_filename.split('.')[0])[0]
     # Construct command to trim the videos (ffmpeg required).
     command = ['ffmpeg',
@@ -122,11 +132,11 @@ def download_clip_wrapper(row, label_to_dir, trim_format, tmp_dir):
                                     tmp_dir=tmp_dir)
     status = tuple([clip_id, downloaded, log])
     return status
- 
 
-def parse_kinetics_annotations(input_csv):
+
+def parse_kinetics_annotations(input_csv, ignore_is_cc=False):
     """Returns a parsed DataFrame.
-    
+
     arguments:
     ---------
     input_csv: str
@@ -140,27 +150,42 @@ def parse_kinetics_annotations(input_csv):
             'video-id', 'start-time', 'end-time', 'label-name'
     """
     df = pd.read_csv(input_csv)
-    df.rename(columns={'youtube_id': 'video-id',
-                       'time_start': 'start-time',
-                       'time_end': 'end-time',
-                       'label': 'label-name',
-                       'is_cc': 'is-cc'}, inplace=True)
+    if 'youtube_id' in df.columns:
+        columns = OrderedDict([
+            ('youtube_id', 'video-id'),
+            ('time_start', 'start-time'),
+            ('time_end', 'end-time'),
+            ('label', 'label-name')])
+        df.rename(columns=columns, inplace=True)
+        if ignore_is_cc:
+            df = df.loc[:, df.columns.tolist()[:-1]]
     return df
 
+
 def main(input_csv, output_dir,
-         trim_format='%06d', num_jobs=24, tmp_dir='/tmp/kinetics'):
+         trim_format='%06d', num_jobs=24, tmp_dir='/tmp/kinetics',
+         drop_duplicates=False):
 
     # Reading and parsing Kinetics.
     dataset = parse_kinetics_annotations(input_csv)
+    # if os.path.isfile(drop_duplicates):
+    #     print('Attempt to remove duplicates')
+    #     old_dataset = parse_kinetics_annotations(drop_duplicates,
+    #                                              ignore_is_cc=True)
+    #     df = pd.concat([dataset, old_dataset], axis=0, ignore_index=True)
+    #     df.drop_duplicates(inplace=True, keep=False)
+    #     print(dataset.shape, old_dataset.shape)
+    #     dataset = df
+    #     print(dataset.shape)
 
     # Creates folders where videos will be saved later.
     label_to_dir = create_video_folders(dataset, output_dir, tmp_dir)
 
     # Download all clips.
-    if num_jobs==1:
+    if num_jobs == 1:
         status_lst = []
         for i, row in dataset.iterrows():
-            status_lst.append(download_clip_wrapper(row, label_to_dir, 
+            status_lst.append(download_clip_wrapper(row, label_to_dir,
                                                     trim_format, tmp_dir))
     else:
         status_lst = Parallel(n_jobs=num_jobs)(delayed(download_clip_wrapper)(
@@ -189,4 +214,7 @@ if __name__ == '__main__':
                          'videoid_%0xd(start_time)_%0xd(end_time).mp4'))
     p.add_argument('-n', '--num-jobs', type=int, default=24)
     p.add_argument('-t', '--tmp-dir', type=str, default='/tmp/kinetics')
+    p.add_argument('--drop-duplicates', type=str, default='non-existent',
+                   help='Unavailable at the moment')
+                   # help='CSV file of the previous version of Kinetics.')
     main(**vars(p.parse_args()))
